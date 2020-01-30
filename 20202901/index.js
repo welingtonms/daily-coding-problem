8d0e8902029051e1943a9395800ebcd701086800
Welington Silva <welington.manoels@gmail.com>
2020-01-30 00:12:04 -0300

    Adding 2020/29/01 problem

---
+++ 20202901/index.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+
+const reader = require('../reader');
+
+function addup(array, k) {
+  const object = array.reduce(function(object, number) {
+    return Object.assign(object, { [number]: true });
+  }, {});
+
+  let found = false;
+  for (let i = 0; i < array.length && !found; i++) {
+    const number = array[i];
+    const diff = Math.abs(number - k);
+
+    if (object[diff]) {
+      found = true;
+    }
+  }
+
+  return found;
+}
+
+module.exports = function() {
+  let stream;
+  try {
+    stream = fs.createWriteStream(__dirname + '/output.txt', { flags: 'a' });
+    const lines = reader(__dirname + '/input.txt');
+
+    for (let i = 0; i < lines.length; i++) {
+      const numbers = lines[i].split(/\s/);
+      i++;
+      const sum = lines[i];
+
+      const result = addup(numbers, sum);
+      stream.write(`${result}\n`);
+    }
+  } catch (err) {
+    console.error(err);
+  } finally {
+    stream && stream.end();
+  }
+};
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# dcp
-My solutions for the Daily Coding Problem (https://www.dailycodingproblem.com/)
+# Daily Coding Problems
+
+My solutions for the Daily Coding Problem (https://www.dailycodingproblem.com/).
+
+Some days I'm more inspired than others, so... pardon me if you don't see those super sophisticated solutions; there's a bit of "enjoying the moment" on these code pieces too.
+
+Enjoy it!
